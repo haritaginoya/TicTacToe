3eb4c2c98f76d1ed537e53b7894d0db61b3e6b21
haritaginoya <haritaginoya@gmail.com>
2024-10-16 11:24:59 +0530

    intial commit

---
+++ app/src/main/java/com/game/tictactoe/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.game.tictactoe
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+//243642
+//387478
+//629584
+//E2F1E7
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
