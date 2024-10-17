c39a4a8b33f2a8b6b50856228f568ff48b26f956
gekyume11 <dhruvharsoda123@gmail.com>
2024-10-17 12:37:12 +0530

    DESIGN PENDING

--- app/src/main/java/com/game/tictactoe/MainActivity.kt
+++ app/src/main/java/com/game/tictactoe/MainActivity.kt
@@ -3,9 +3,16 @@ package com.game.tictactoe
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.view.animation.AnimationUtils
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +27,6 @@ class MainActivity : AppCompatActivity() {
         R.id.btn_7,
         R.id.btn_8
     )
-    private val tie = 0;
     private var value = true
     var play = true
 
@@ -30,15 +36,33 @@ class MainActivity : AppCompatActivity() {
 
         for (i in 0..8) btnClick(i)
 
+        findViewById<LinearLayout>(R.id.refresh).setOnClickListener {
+            refresh()
+        }
+
+    }
+
+    fun refresh() {
+        CoroutineScope(Dispatchers.Main).launch {
+            for (i in 0 until arrayOfbtnId.size) {
+                findViewById<TextView>(arrayOfbtnId[i]).setTextSize(TypedValue.COMPLEX_UNIT_SP, 60f)
+                findViewById<TextView>(arrayOfbtnId[i]).text = ""
+                delay(80)
+            }
+        }
+        value = true
     }
 
     fun btnClick(index: Int) {
 
         val btn = findViewById<TextView>(arrayOfbtnId[index])
+
         btn.setOnClickListener {
 
             if (btn.text.isEmpty() && play) {
+
                 if (value) {
+                    btn.setTextColor(Color.parseColor("#F3C623"))
                     btn.text = "X"
                     value = false
                 } else {
@@ -46,18 +70,35 @@ class MainActivity : AppCompatActivity() {
                     btn.text = "0"
                     value = true
                 }
+
+                checkWinner(0, 1, 2)
+                checkWinner(3, 4, 5)
+                checkWinner(6, 7, 8)
+                checkWinner(0, 3, 6)
+                checkWinner(1, 4, 7)
+                checkWinner(2, 5, 8)
+                checkWinner(0, 4, 8)
+                checkWinner(2, 4, 6)
+                checkTie()
             }
+        }
+    }
 
-            checkWinner(0, 1, 2)
-            checkWinner(3, 4, 5)
-            checkWinner(6, 7, 8)
-            checkWinner(0, 3, 6)
-            checkWinner(1, 4, 7)
-            checkWinner(2, 5, 8)
-            checkWinner(0, 4, 8)
-            checkWinner(2, 4, 6)
-
-
+    @SuppressLint("CutPasteId", "SetTextI18n")
+    fun checkTie() {
+        CoroutineScope(Dispatchers.Main).launch {
+            for (i in arrayOfbtnId.indices) {
+                if (findViewById<TextView>(arrayOfbtnId[i]).text.isEmpty()) {
+                    break
+                }
+                if (i == (arrayOfbtnId.size - 1) && play) {
+                    Log.d("----t----", "checkWinner: tie")
+                    val tie = findViewById<TextView>(R.id.tie).text.toString().toInt()
+                    findViewById<TextView>(R.id.tie).text = "${tie + 1}"
+                    delay(3000)
+                    refresh()
+                }
+            }
         }
     }
 
@@ -78,9 +119,37 @@ class MainActivity : AppCompatActivity() {
                 val player_0 = findViewById<TextView>(R.id.player_0).text.toString().toInt()
                 findViewById<TextView>(R.id.player_0).text = "${player_0 + 1}"
             }
-            b1.setBackgroundColor(Color.parseColor("#E2F1E7"))
+            val animation = AnimationUtils.loadAnimation(this, R.anim.zoom)
+
+            CoroutineScope(Dispatchers.Main).launch {
+
+                b1.setBackgroundColor(Color.parseColor("#C62E2E"))
+                delay(300)
+                b2.setBackgroundColor(Color.parseColor("#C62E2E"))
+                delay(300)
+                b3.setBackgroundColor(Color.parseColor("#C62E2E"))
+
+                b1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40f)
+                b2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40f)
+                b3.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40f)
+
+                b1.startAnimation(animation)
+                b2.startAnimation(animation)
+                b3.startAnimation(animation)
+
+                delay(3000)
+
+                b1.setBackgroundColor(Color.parseColor("#243642"))
+                b2.setBackgroundColor(Color.parseColor("#243642"))
+                b3.setBackgroundColor(Color.parseColor("#243642"))
+                refresh()
+
+                b1.clearAnimation()
+                b2.clearAnimation()
+                b3.clearAnimation()
+
+                play = true
+            }
         }
-
     }
-
 }
